f37ced70e6e29aeeff4b8505c9ae9fb633aec0b9
juliana-nnunes <josecarloslimanunes30@gmail.com>
2025-10-07 19:36:48 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿            double m, cm, Km;
+
+            Console.Write("Entre com a medida (em metros): ");
+            m = Convert.ToDouble(Console.ReadLine());
+
+            cm = m * 100;
+            Km = m / 1000;
+
+            Console.WriteLine("--- Equivalencia ---");
+            Console.WriteLine($"{cm}cm ");
+            Console.WriteLine($"{m} m ");
+            Console.WriteLine($"{Km} km");
+
+
